278d7b58be1ea6d60189fe6a12c353b77d71de10
DhruvaTikhe <college.dhruvatikhe@gmail.com>
2026-08-26 13:32:22 +0530

    Updated logic for scaling.py

--- preprocessing/scaling.py
+++ preprocessing/scaling.py
@@ -1,18 +1,35 @@
+from sklearn.preprocessing import MinMaxScaler, RobustScaler
+
 TITLE = "Scaling"
 
-COLUMN_TYPE = "numeric"      # all, numeric, categorical, datetime
+COLUMN_TYPE = "numeric"
 
 METHODS = [
-    "SC Meth 1",
-    "SC Meth 2",
-    "SC Meth 3",
-    "SC Meth 4"
+    "Min-Max Scaling",
+    "Robust Scaling"
 ]
 
 SHOW_CONSTANT = False
 ALLOW_MULTISELECT = True
 
 
-# def scaling(df, columns, method, **kwargs):
+def process(df, columns, method, **kwargs):
 
-#     return df, log
+    if not columns:
+        return df, "No columns selected for scaling."
+
+    if method == "Min-Max Scaling":
+        scaler = MinMaxScaler() #(x-xmin / xmax-xmin)
+
+    elif method == "Robust Scaling":
+        scaler = RobustScaler() #(x-median(x) / IQR(x))
+
+    else:
+        return df, f"Unknown scaling method: {method}"
+
+    # Fit and transform selected columns
+    df[columns] = scaler.fit_transform(df[columns])
+
+    log = f"{method} applied to {len(columns)} column(s): {', '.join(columns)}"
+
+    return df, log
